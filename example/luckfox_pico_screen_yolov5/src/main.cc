fa4605885fbb9408022c7cb388330d415615c423
hydrogenbond001 <3101383697@qq.com>
2025-03-26 04:53:16 +0800

    screen suc

--- example/luckfox_pico_screen_yolov5/src/main.cc
+++ example/luckfox_pico_screen_yolov5/src/main.cc
@@ -1,7 +1,6 @@
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <getopt.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -13,7 +12,6 @@
 #include <unistd.h>
 #include <vector>
 
-#include "rtsp_demo.h"
 #include "luckfox_mpi.h"
 #include "yolov5.h"
 
@@ -21,234 +19,249 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-#define DISP_WIDTH  720
+#include <drm/drm.h>
+#include <drm/drm_mode.h>
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+#include <iostream>
+#include <sys/mman.h>
+
+#define DISP_WIDTH  480
 #define DISP_HEIGHT 480
 
-// disp size
+// Display size
 int width    = DISP_WIDTH;
 int height   = DISP_HEIGHT;
 
-// model size
+// Model size
 int model_width = 640;
-int model_height = 640;	
-float scale ;
-int leftPadding ;
-int topPadding  ;
+int model_height = 640;    
+float scale;
+int leftPadding;
+int topPadding;
 
-cv::Mat letterbox(cv::Mat input)
-{
-	float scaleX = (float)model_width  / (float)width; 
-	float scaleY = (float)model_height / (float)height; 
-	scale = scaleX < scaleY ? scaleX : scaleY;
-	
-	int inputWidth   = (int)((float)width * scale);
-	int inputHeight  = (int)((float)height * scale);
+cv::Mat letterbox(cv::Mat input) {
+    float scaleX = (float)model_width / (float)width;
+    float scaleY = (float)model_height / (float)height;
+    scale = scaleX < scaleY ? scaleX : scaleY;
+    
+    int inputWidth  = (int)((float)width * scale);
+    int inputHeight = (int)((float)height * scale);
 
-	leftPadding = (model_width  - inputWidth) / 2;
-	topPadding  = (model_height - inputHeight) / 2;	
-	
-
-	cv::Mat inputScale;
-    cv::resize(input, inputScale, cv::Size(inputWidth,inputHeight), 0, 0, cv::INTER_LINEAR);	
-	cv::Mat letterboxImage(640, 640, CV_8UC3,cv::Scalar(0, 0, 0));
+    leftPadding = (model_width - inputWidth) / 2;
+    topPadding  = (model_height - inputHeight) / 2;
+    
+    cv::Mat inputScale;
+    cv::resize(input, inputScale, cv::Size(inputWidth, inputHeight), 0, 0, cv::INTER_LINEAR);
+    cv::Mat letterboxImage(640, 640, CV_8UC3, cv::Scalar(0, 0, 0));
     cv::Rect roi(leftPadding, topPadding, inputWidth, inputHeight);
     inputScale.copyTo(letterboxImage(roi));
 
-	return letterboxImage; 	
+    return letterboxImage;
 }
 
-void mapCoordinates(int *x, int *y) {	
-	int mx = *x - leftPadding;
-	int my = *y - topPadding;
-
+void mapCoordinates(int *x, int *y) {
+    int mx = *x - leftPadding;
+    int my = *y - topPadding;
     *x = (int)((float)mx / scale);
     *y = (int)((float)my / scale);
+}
+static uint64_t get_time_ms() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (uint64_t)ts.tv_sec * 1000 + (uint64_t)ts.tv_nsec / 1000000;
 }
 
 
 int main(int argc, char *argv[]) {
-  system("RkLunch-stop.sh");
-	RK_S32 s32Ret = 0; 
-	int sX,sY,eX,eY; 
+    // Initialize YOLOv5 model
+    rknn_app_context_t rknn_app_ctx;
+    object_detect_result_list od_results;
+    memset(&rknn_app_ctx, 0, sizeof(rknn_app_context_t));
+    init_yolov5_model("./model/yolov5.rknn", &rknn_app_ctx);
+    init_post_process();
+
+    uint32_t fb_id = 0;
+
+    // DRM setup
+    int fd = open("/dev/dri/card0", O_RDWR);
+    drmModeRes *resources = drmModeGetResources(fd);
+    drmModeConnector *connector = nullptr;
+    for (int i = 0; i < resources->count_connectors; i++) {
+        connector = drmModeGetConnector(fd, resources->connectors[i]);
+        if (connector && connector->connection == DRM_MODE_CONNECTED) {
+            break;
+        }
+        drmModeFreeConnector(connector);
+    }
+
+    drmModeModeInfo *mode = NULL;
+	for (int i = 0; i < connector->count_modes; i++) {
+        if (connector->modes[i].hdisplay == DISP_WIDTH &&
+            connector->modes[i].vdisplay == DISP_HEIGHT) {
+            mode = &connector->modes[i];
+            break;
+        }
+    }
+
+	if (!mode) {
+        fprintf(stderr, "No matching display mode found.\n");
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+
+	
+
+
+	// 创建 Dumb Buffer
+    struct drm_mode_create_dumb create_dumb = {0};
+    create_dumb.width = DISP_WIDTH;
+    create_dumb.height = DISP_HEIGHT;
+    create_dumb.bpp = 32; // 32 bits per pixel (ARGB8888)
+
+    if (drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &create_dumb) < 0) {
+        fprintf(stderr, "Failed to create dumb buffer: %s\n", strerror(errno));
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+    
+    // 映射 Dumb Buffer
+    struct drm_mode_map_dumb map_dumb = {0};
+    map_dumb.handle = create_dumb.handle;
+
+    if (drmIoctl(fd, DRM_IOCTL_MODE_MAP_DUMB, &map_dumb) < 0) {
+        fprintf(stderr, "Failed to map dumb buffer: %s\n", strerror(errno));
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+    void *fb_map = mmap(0, create_dumb.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, map_dumb.offset);
+    if (fb_map == MAP_FAILED) {
+        fprintf(stderr, "Failed to mmap framebuffer: %s\n", strerror(errno));
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+    // 创建 DRM Framebuffer
+    if (drmModeAddFB(fd, DISP_WIDTH, DISP_HEIGHT, 24, 32, create_dumb.pitch, create_dumb.handle, &fb_id) < 0) {
+        fprintf(stderr, "Failed to add framebuffer: %s\n", strerror(errno));
+        munmap(fb_map, create_dumb.size);
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+    // 设置 CRTC（显示控制器）
+    drmModeCrtc *crtc = drmModeGetCrtc(fd, resources->crtcs[0]);
+    if (!crtc) {
+        fprintf(stderr, "Failed to get CRTC: %s\n", strerror(errno));
+        munmap(fb_map, create_dumb.size);
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+    if (drmModeSetCrtc(fd, crtc->crtc_id, fb_id, 0, 0, &connector->connector_id, 1, mode) < 0) {
+        fprintf(stderr, "Failed to set CRTC: %s\n", strerror(errno));
+        drmModeFreeCrtc(crtc);
+        munmap(fb_map, create_dumb.size);
+        drmModeFreeConnector(connector);
+        drmModeFreeResources(resources);
+        close(fd);
+        return -1;
+    }
+
+	
+	uint64_t start_time = get_time_ms();
+	
+    // Video capture and inference loop
+    // cv::VideoCapture cap(0);
+	cv::Mat frame = cv::imread("./model/1234.jpg"); 
+    // if (!cap.isOpened()) {
+    //     printf("Failed to open camera\n");
+    //     return -1;
+    // }
+    if (frame.empty()) {
+        std::cerr << "无法加载图片！请检查路径是否正确。" << std::endl;
+        return -1;
+    }
+    // while (true) {
+        // Capture frame
+        // cv::Mat frame;
+        // cap >> frame;
+        // if (frame.empty()) {
+        //     continue;
+        // }
+        
+        cv::Mat letterboxImage = letterbox(frame);
+        memcpy(rknn_app_ctx.input_mems[0]->virt_addr, letterboxImage.data, model_width * model_height * 3);
+        inference_yolov5_model(&rknn_app_ctx, &od_results);
+
+        for (int i = 0; i < od_results.count; i++) {
+            object_detect_result *det_result = &(od_results.results[i]);
+            int sX = (int)(det_result->box.left);
+            int sY = (int)(det_result->box.top);
+            int eX = (int)(det_result->box.right);
+            int eY = (int)(det_result->box.bottom);
+            mapCoordinates(&sX, &sY);
+            mapCoordinates(&eX, &eY);
+            cv::rectangle(frame, cv::Point(sX, sY), cv::Point(eX, eY), cv::Scalar(0, 255, 0), 3);
+        }
+		cv::cvtColor(frame, frame, cv::COLOR_BGR2BGRA);
+		cv::resize(frame, frame, cv::Size(DISP_WIDTH, DISP_HEIGHT));
 		
-	// Rknn model
-	char text[16];
-	rknn_app_context_t rknn_app_ctx;	
-	object_detect_result_list od_results;
-    int ret;
-	const char *model_path = "./model/yolov5.rknn";
-    memset(&rknn_app_ctx, 0, sizeof(rknn_app_context_t));	
-	init_yolov5_model(model_path, &rknn_app_ctx);
-	printf("init rknn model success!\n");
-	init_post_process();
-
-	//h264_frame	
-	VENC_STREAM_S stFrame;	
-	stFrame.pstPack = (VENC_PACK_S *)malloc(sizeof(VENC_PACK_S));
-	RK_U64 H264_PTS = 0;
-	RK_U32 H264_TimeRef = 0; 
-	VIDEO_FRAME_INFO_S stViFrame;
-	
-	// Create Pool
-	MB_POOL_CONFIG_S PoolCfg;
-	memset(&PoolCfg, 0, sizeof(MB_POOL_CONFIG_S));
-	PoolCfg.u64MBSize = width * height * 3 ;
-	PoolCfg.u32MBCnt = 1;
-	PoolCfg.enAllocType = MB_ALLOC_TYPE_DMA;
-	//PoolCfg.bPreAlloc = RK_FALSE;
-	MB_POOL src_Pool = RK_MPI_MB_CreatePool(&PoolCfg);
-	printf("Create Pool success !\n");	
-
-	// Get MB from Pool 
-	MB_BLK src_Blk = RK_MPI_MB_GetMB(src_Pool, width * height * 3, RK_TRUE);
-	
-	// Build h264_frame
-	VIDEO_FRAME_INFO_S h264_frame;
-	h264_frame.stVFrame.u32Width = width;
-	h264_frame.stVFrame.u32Height = height;
-	h264_frame.stVFrame.u32VirWidth = width;
-	h264_frame.stVFrame.u32VirHeight = height;
-	h264_frame.stVFrame.enPixelFormat =  RK_FMT_RGB888; 
-	h264_frame.stVFrame.u32FrameFlag = 160;
-	h264_frame.stVFrame.pMbBlk = src_Blk;
-	unsigned char *data = (unsigned char *)RK_MPI_MB_Handle2VirAddr(src_Blk);
-	cv::Mat frame(cv::Size(width,height),CV_8UC3,data);
-
-	// rkaiq init
-	RK_BOOL multi_sensor = RK_FALSE;	
-	const char *iq_dir = "/etc/iqfiles";
-	rk_aiq_working_mode_t hdr_mode = RK_AIQ_WORKING_MODE_NORMAL;
-	//hdr_mode = RK_AIQ_WORKING_MODE_ISP_HDR2;
-	SAMPLE_COMM_ISP_Init(0, hdr_mode, multi_sensor, iq_dir);
-	SAMPLE_COMM_ISP_Run(0);
-
-	// rkmpi init
-	if (RK_MPI_SYS_Init() != RK_SUCCESS) {
-		RK_LOGE("rk mpi sys init fail!");
-		return -1;
-	}
-
-	// rtsp init	
-	rtsp_demo_handle g_rtsplive = NULL;
-	rtsp_session_handle g_rtsp_session;
-	g_rtsplive = create_rtsp_demo(554);
-	g_rtsp_session = rtsp_new_session(g_rtsplive, "/live/0");
-	rtsp_set_video(g_rtsp_session, RTSP_CODEC_ID_VIDEO_H264, NULL, 0);
-	rtsp_sync_video_ts(g_rtsp_session, rtsp_get_reltime(), rtsp_get_ntptime());
-	
-	// vi init
-	vi_dev_init();
-	vi_chn_init(0, width, height);
-
-	// venc init
-	RK_CODEC_ID_E enCodecType = RK_VIDEO_ID_AVC;
-	venc_init(0, width, height, enCodecType);
-
-	printf("venc init success\n");	
-	
-  	while(1)
-	{	
-		// get vi frame
-		h264_frame.stVFrame.u32TimeRef = H264_TimeRef++;
-		h264_frame.stVFrame.u64PTS = TEST_COMM_GetNowUs(); 
-		s32Ret = RK_MPI_VI_GetChnFrame(0, 0, &stViFrame, -1);
-		if(s32Ret == RK_SUCCESS)
-		{
-			void *vi_data = RK_MPI_MB_Handle2VirAddr(stViFrame.stVFrame.pMbBlk);	
-
-			cv::Mat yuv420sp(height + height / 2, width, CV_8UC1, vi_data);
-			cv::Mat bgr(height, width, CV_8UC3, data);			
-			
-			cv::cvtColor(yuv420sp, bgr, cv::COLOR_YUV420sp2BGR);
-			cv::resize(bgr, frame, cv::Size(width ,height), 0, 0, cv::INTER_LINEAR);
-			
-			//letterbox
-			cv::Mat letterboxImage = letterbox(frame);	
-			memcpy(rknn_app_ctx.input_mems[0]->virt_addr, letterboxImage.data, model_width*model_height*3);		
-			inference_yolov5_model(&rknn_app_ctx, &od_results);
-
-			for(int i = 0; i < od_results.count; i++)
-			{					
-				if(od_results.count >= 1)
-				{
-					object_detect_result *det_result = &(od_results.results[i]);
-	
-					sX = (int)(det_result->box.left   );	
-					sY = (int)(det_result->box.top 	  );	
-					eX = (int)(det_result->box.right  );	
-					eY = (int)(det_result->box.bottom );
-					mapCoordinates(&sX,&sY);
-					mapCoordinates(&eX,&eY);
-					
-					printf("%s @ (%d %d %d %d) %.3f\n", coco_cls_to_name(det_result->cls_id),
-							 sX, sY, eX, eY, det_result->prop);
-
-					cv::rectangle(frame,cv::Point(sX ,sY),
-								        cv::Point(eX ,eY),
-										cv::Scalar(0,255,0),3);
-					sprintf(text, "%s %.1f%%", coco_cls_to_name(det_result->cls_id), det_result->prop * 100);
-					cv::putText(frame,text,cv::Point(sX, sY - 8),
-										   cv::FONT_HERSHEY_SIMPLEX,1,
-										   cv::Scalar(0,255,0),2);
-				}
-			}
-
-		}
-		memcpy(data, frame.data, width * height * 3);					
+		// 9. 复制 RGBA 数据到 framebuffer
+		memcpy(fb_map, frame.data, create_dumb.size);
 		
-		// encode H264
-		RK_MPI_VENC_SendFrame(0, &h264_frame,-1);
+    
+    // 缩放或裁剪以匹配 SCREEN_WIDTH x SCREEN_HEIGHT
+    // for (int y = 0; y < SCREEN_HEIGHT; y++) {
+    //     for (int x = 0; x < SCREEN_WIDTH; x++) {
+    //         int img_x = (x * img_width) / SCREEN_WIDTH;
+    //         int img_y = (y * img_height) / SCREEN_HEIGHT;
+    //         int pixel_idx = (img_y * img_width + img_x) * 4;
+    
+    //         uint8_t r = img_data[pixel_idx];
+    //         uint8_t g = img_data[pixel_idx + 1];
+    //         uint8_t b = img_data[pixel_idx + 2];
+    //         uint8_t a = img_data[pixel_idx + 3];
+    
+    //         ((uint32_t*)fb_map)[y * SCREEN_WIDTH + x] = (a << 24) | (r << 16) | (g << 8) | b;
+    //     }
+    // }
+    
+    // drmModeCrtc *crtc = drmModeGetCrtc(fd, connector->encoder_id);
+    drmModeSetCrtc(fd, crtc->crtc_id, fb_id, 0, 0, &connector->connector_id, 1, mode);
 
-		// rtsp
-		s32Ret = RK_MPI_VENC_GetStream(0, &stFrame, -1);
-		if(s32Ret == RK_SUCCESS)
-		{
-			if(g_rtsplive && g_rtsp_session)
-			{
-				//printf("len = %d PTS = %d \n",stFrame.pstPack->u32Len, stFrame.pstPack->u64PTS);	
-				void *pData = RK_MPI_MB_Handle2VirAddr(stFrame.pstPack->pMbBlk);
-				rtsp_tx_video(g_rtsp_session, (uint8_t *)pData, stFrame.pstPack->u32Len,
-							  stFrame.pstPack->u64PTS);
-				rtsp_do_event(g_rtsplive);
-			}
-		}
+    // }
+	uint64_t end_time = get_time_ms();
 
-		// release frame 
-		s32Ret = RK_MPI_VI_ReleaseChnFrame(0, 0, &stViFrame);
-		if (s32Ret != RK_SUCCESS) {
-			RK_LOGE("RK_MPI_VI_ReleaseChnFrame fail %x", s32Ret);
-		}
-		s32Ret = RK_MPI_VENC_ReleaseStream(0, &stFrame);
-		if (s32Ret != RK_SUCCESS) {
-			RK_LOGE("RK_MPI_VENC_ReleaseStream fail %x", s32Ret);
-		}
-		memset(text,0,8);
-	}
+	double total_time = (end_time - start_time) / 1000.0;  // 转换为秒
+    printf("cost %.2f seconds\n", total_time);
 
+    while (getchar() != 'q'); // 循环等待用户输入'q'，然后退出
 
-	// Destory MB
-	RK_MPI_MB_ReleaseMB(src_Blk);
-	// Destory Pool
-	RK_MPI_MB_DestroyPool(src_Pool);
-	
-	RK_MPI_VI_DisableChn(0, 0);
-	RK_MPI_VI_DisableDev(0);
+    // Cleanup
+    release_yolov5_model(&rknn_app_ctx);
+    deinit_post_process();
 
-	SAMPLE_COMM_ISP_Stop(0);
-	
-	RK_MPI_VENC_StopRecvFrame(0);
-	RK_MPI_VENC_DestroyChn(0);
-
-	free(stFrame.pstPack);
-
-	if (g_rtsplive)
-		rtsp_del_demo(g_rtsplive);
-	
-	RK_MPI_SYS_Exit();
-
-	// Release rknn model
-    release_yolov5_model(&rknn_app_ctx);		
-	deinit_post_process();
-	
-	return 0;
+	drmModeRmFB(fd, fb_id);
+    munmap(fb_map, create_dumb.size);
+    drmModeFreeCrtc(crtc);
+    drmModeFreeConnector(connector);
+    drmModeFreeResources(resources);
+    close(fd);
+    return 0;
 }
